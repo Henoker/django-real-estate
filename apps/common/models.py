69d89589f93a8c7525bbd96fe1454107b2c5afb7
Henoker <88382381+Henoker@users.noreply.github.com>
2022-09-14 19:06:28 +0300

    feat: add rating model and serializers

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,13 +1,13 @@
-from django.db import models
 import uuid
 
-# Create your models here.
+from django.db import models
+
+
 class TimeStampedUUIDModel(models.Model):
     pkid = models.BigAutoField(primary_key=True, editable=False)
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    created_id = models.DateTimeField(auto_now_add=True)
-    updated_id = models.DateTimeField(auto_now=True)    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        abstract: True
-        
+        abstract = True
